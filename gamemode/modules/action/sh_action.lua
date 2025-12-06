b19aa1580e9e9cbd4b82b0369889bebe61570ee7
CursedSC <103887456+CursedSC@users.noreply.github.com>
2025-12-06 10:44:13 +0300

    Добавлена автозагрузка файла мини-игры медикаментов

--- gamemode/modules/action/sh_action.lua
+++ gamemode/modules/action/sh_action.lua
@@ -8,3 +8,7 @@ if SERVER then
     util.AddNetworkString("dbt.ApplyMedication")
     util.AddNetworkString("dbt.OpenMedicationMenu")
 end
+
+if CLIENT then
+    include("cl_medication_minigame.lua")
+end
