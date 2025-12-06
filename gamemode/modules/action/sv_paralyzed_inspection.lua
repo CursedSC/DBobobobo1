639e25973d29346facb646daca3d7dc703da23bf
CursedSC <103887456+CursedSC@users.noreply.github.com>
2025-12-06 14:55:32 +0300

    Обновлена система осмотра парализованных: вывод информации в чат

--- gamemode/modules/action/sv_paralyzed_inspection.lua
+++ gamemode/modules/action/sv_paralyzed_inspection.lua
@@ -49,55 +49,21 @@ hook.Add("PlayerUse", "dbt.InspectParalyzedPlayer", function(ply, ent)
     if not IsValid(ply) or not IsValid(ent) then return end
     
     if ent:IsPlayer() then
+        local distance = ply:GetPos():Distance(ent:GetPos())
+        if distance > 150 then return end
+        
         local info = GetParalyzedPlayerInfo(ent)
         if info then
-            net.Start("dbt.ShowParalyzedInfo")
-                net.WriteString(info.name)
-                net.WriteBool(info.isParalyzed)
-                net.WriteString(info.drug)
-                net.WriteString(info.drugType)
-                net.WriteString(info.bodyPart)
-                net.WriteString(info.timeRemaining)
-                net.WriteString(info.status)
-            net.Send(ply)
+            ply:ChatPrint("═══════════════════════════════════")
+            ply:ChatPrint("Осмотр парализованного игрока")
+            ply:ChatPrint("═══════════════════════════════════")
+            ply:ChatPrint("Имя: " .. info.name)
+            ply:ChatPrint("Статус: " .. info.status)
+            ply:ChatPrint("Препарат: " .. info.drug)
+            ply:ChatPrint("Часть тела: " .. info.bodyPart)
+            ply:ChatPrint("Осталось времени: " .. info.timeRemaining)
+            ply:ChatPrint("═══════════════════════════════════")
             return true
         end
-    end
-end)
-
-net.Receive("dbt.InspectParalyzedPlayer", function(len, ply)
-    local target = net.ReadEntity()
-    
-    if not IsValid(ply) or not IsValid(target) then return end
-    
-    local distance = ply:GetPos():Distance(target:GetPos())
-    if distance > 150 then
-        netstream.Start(ply, 'dbt/NewNotification', 3, {
-            icon = 'materials/dbt/notifications/notifications_main.png', 
-            title = 'Уведомление', 
-            titlecolor = Color(222, 193, 49), 
-            notiftext = 'Цель слишком далеко!'
-        })
-        return
-    end
-    
-    local info = GetParalyzedPlayerInfo(target)
-    if info then
-        net.Start("dbt.ShowParalyzedInfo")
-            net.WriteString(info.name)
-            net.WriteBool(info.isParalyzed)
-            net.WriteString(info.drug)
-            net.WriteString(info.drugType)
-            net.WriteString(info.bodyPart)
-            net.WriteString(info.timeRemaining)
-            net.WriteString(info.status)
-        net.Send(ply)
-    else
-        netstream.Start(ply, 'dbt/NewNotification', 3, {
-            icon = 'materials/dbt/notifications/notifications_main.png', 
-            title = 'Осмотр', 
-            titlecolor = Color(82, 204, 117), 
-            notiftext = 'Игрок ' .. target:Nick() .. ' не парализован.'
-        })
     end
 end)
